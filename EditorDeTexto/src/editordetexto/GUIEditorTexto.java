c1fe27579ed076c50480745683b307b58df10086
rafacastillo2006 <raficastillo2006@gmail.com>
2026-09-05 08:50:04 -0600

    Restaurar carpetas

--- EditorDeTexto/src/editordetexto/GUIEditorTexto.java
+++ EditorDeTexto/src/editordetexto/GUIEditorTexto.java
@@ -1,16 +1,14 @@
 import javax.swing.*;
-import javax.swing.text.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 
-public class GUIEditordeTexto extends JFrame {
+public class GUIEditorTexto extends JFrame {
     private JTextPane areaTexto;
     private JComboBox<String> Fuentes;
     private JComboBox<Integer> Tamanos;
     private JToggleButton btnBOLD, btnITALIC, btnSUBRAYAR, btnTACHAR;
 
 
-    public GUIEditordeTexto() {
+    public GUIEditorTexto() {
         setTitle("Procesador de Texto .EDT");
         setSize(900, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
